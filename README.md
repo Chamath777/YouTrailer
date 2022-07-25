aa421b81e29f1bec2c82b87c8c756a8c19606f35
Chamath <imac@192-168-1-3.tpgi.com.au>
2022-07-25 21:39:55 +0930

    youtrailer

---
+++ README.md
@@ -0,0 +1,133 @@
+# YouTrailer
+## User Story
+
+AS A person who enjoys discovering and watching entertainment, 
+
+I WANT TO be able to search for entertainment and view trailers
+
+SO THAT I can decide what I would like to watch or play next.
+
+
+# About YouTrailer
+
+YouTrailer is a library of trailers for different types of entertainment, whether it is movies, tv shows, video games or other sources of video entertainment. 
+
+The site is used to browse movies by category, year of release and rating, or by title that you are searching for (can even search without a title).
+
+So users can find movies according to desired search and view the trailer.
+
+
+## How YouTrailer built 🛠 
+
+# Mock
+
+  ![sketch](./images/PXL_20220714_090411275.jpg) 
+
+# Tools used
+   
+    🔗 HTML
+    
+    🔗 CSS 
+
+    🔗 Bulma
+    
+    🔗 Javascript
+
+    🔗 JQuery
+
+    // Libraries used,
+
+        - Jquery (https://jquery.com/)
+
+        - IMDb API (https://imdb-api.com/api)
+
+        - YouTube API (https://cloud.google.com/)
+
+        - FontAwesome (https://fontawesome.com/start)
+
+    // Framework - Bulma (https://bulma.io/)
+
+    // Other Sources used,
+
+        - Google (https://www.google.com/)
+
+
+// YouTrailer was developed to give maximum user friendliness to the browser. The site is designed with multiple filters for the best user accessibility. Features and functionalities as follows;
+
+![sketch](./images/Screenshot-main.jpg)
+
+# Features of,
+
+    @ Search section -  
+
+    This is the section where user can enter the title of the desired entertainment and click the search button and the main page will show all the relevant entertainment selections similar to the title entered.  
+
+    @ Search History - 
+
+    Search history is designed to save browser time. Once the user input the title of their entertainment, it stores in the search bar. So user can easily search the same name of the movie without re-entering into the search bar.  
+
+    @ Choosing the Genre -
+
+    User also can search the trailer by choosing a Genre. This is, if the user wants to search the same title and also can be searched by the category. This option has been designed due to some other entertainment related to multiple categories.
+
+    Ex:
+
+    @ Year of Made -
+
+    If there's a series of entertainment under one title which are released in different years, the user can search the particular title by year of released and get the filtered results.
+
+    Ex:
+
+    @ Rating -
+
+    Browser can filter the searched results by choosing the range of the rating 1 to 10.
+
+![sketch](./images/Screenshot-titles.jpg)
+
+# Functionalities of,
+
+    @ Search Section -
+ 
+    In this section, the titles the user has selected has been saved within the local storage using the JSON stringify and parse functions. 
+
+    When the browser is refreshed the history list will be presented due to the function of parsing the local storage and it is present on the User Interface (UI). 
+
+    @ API -
+    
+    Clicking on the title in the history section calls on the youtube API to receive the URL and creates a youtube video player on the main screen.
+   
+    The search section also triggers the API call by inputting the search name into the API request url. When the user clicked a title it will be stored within a variable and then stored within the local storage using stringify. 
+
+![sketch](./images/Screenshot-youtube.jpg)
+
+# Obstacles 🤔
+
+    - Git command failures
+
+    - Git merging failures
+
+    - Imdb errors
+
+    - API key validation period
+
+    - YouTube API key validation period
+
+    - Bulma coding pattern
+
+
+# Authors
+
+    - Ben (HTML, CSS, JavaScript & API)
+
+    - Roman (HTML, CSS, JavaScript & API)
+
+    - Caroline (Sketch, HTML & CSS)
+
+    - Chamath (HTML, README) 
+
+⚡️ Fun fact...
+
+Thank you for the Developers for making us Developers!
+
+## Deployed URL
+https://carolinemae.github.io/YouTrailer/ 
---
+++ assets/script.js
@@ -0,0 +1,223 @@
+// Global Variables
+var searchButton = $("#searchButton");
+var apiKey = "k_q5dx85dn"; //  k_2fn865ld k_yu9dk0350 
+var youtubePreview = $(".preview");
+var previewInfo = $("#previewInfo");
+var movieResult;
+var movieInfo = $('#movie-info');
+var counter = 0;
+var savedLocal = [];
+var closeButton = $("#closebutton");
+var homepageDesc = $("#homepage-desc");
+
+// Slider creation on UI
+var sliderRating = document.getElementById("myRangeRating");
+var outputRating = document.getElementById("rating");
+outputRating.innerHTML = sliderRating.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+sliderRating.oninput = function() {
+var ratingLow = outputRating.innerHTML = this.value;
+};
+
+var sliderYear = document.getElementById("myRangeYear");
+var outputYear = document.getElementById("year");
+outputYear.innerHTML = sliderYear.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+sliderYear.oninput = function() {
+var yearLow = outputYear.innerHTML = this.value;
+};
+
+var genres = $('#genre :selected').text();
+
+for (var i = 0; i < localStorage.length; i++) {
+    var title = localStorage.getItem(i);
+};
+var keyCount = 0;
+
+// Function that takes in title, year and rating and requests to the IMDB API
+function searchMovie(title, year, rating) {
+    var rating = $("#rating").text();
+    var year = $("#year").text();
+    var genres = $('#genre :selected').text();
+
+    //APi Link goes here
+    var urlMovie = "https://imdb-api.com/API/AdvancedSearch/" + apiKey + "?title=" + title + "&user_rating=" + rating + ",10&release_date=" + year + "-01-01,2022-01-01&genres=" + genres;
+    
+    $.ajax({
+        url: urlMovie,
+        method: "GET"
+    }).then(function (response) {
+        var response = response.results;
+        movieResult = $("#movies").append("<div>");
+        movieResult.empty();
+        createCard(response);
+    })
+};
+
+// Function that takes in the title chosen and fetches the YouTube URL and then calls function with the data to create the link
+const getYoutubeApi = (title, year) => {
+    var titleCheck = checkTitleSpaces(title);
+    var youtubeUrl = `https://www.googleapis.com/youtube/v3/search?q=${titleCheck}%20${year}%20official%20trailer&key=AIzaSyADmSfBWFvQzs61upAmVe6_i-JemetTXY0`;
+    fetch(youtubeUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var movieYoutubeId = data.items[0].id.videoId;
+            createYoutubeSection(movieYoutubeId);
+        })
+        homepageDesc.addClass('hidden');
+};
+
+// Function that checks the title if it has spaces and returns correct youtube API formatting
+const checkTitleSpaces = title => {
+    if (title.includes(' ')) {
+        var spaceTitle = title.replaceAll(' ', '%20');
+        return spaceTitle;
+    } else {
+        return title;
+    };
+};
+
+// Function intake the API response and then creates elements of the Movie Cards containing- title, year, rating and poster image
+const createCard = response => {
+    var movieTitle;
+    var movieYear;
+    var movieRating;
+    var movieImage;
+    var clickedTitle;
+    var counter = 0;
+    
+    for (var i = 0; i < 31; i ++){
+        var movieCard = $('<div>');
+        var movieTitleInput = $('<h3>');
+        var movieYearInput = $('<p>');
+        var movieRatingInput =$('<p>');
+        var movieImgInput = $('<img>');
+        counter++;
+
+        movieCard.addClass('movie-card');
+        movieResult.append(movieCard);
+
+        movieTitle = response[i].title;
+        movieTitleInput.text(movieTitle);
+        movieTitleInput.addClass('movie-title');
+        movieTitleInput.addClass(`movie-${counter}`);
+        movieCard.append(movieTitleInput);
+
+        movieYear = response[i].description;
+        movieYearInput.text(movieYear);
+        movieYearInput.addClass('movie-year');
+        movieCard.append(movieYearInput);
+
+        movieRating = response[i].imDbRating;
+        movieRatingInput.text(movieRating);
+        movieRatingInput.addClass('movie-rating');
+        movieCard.append(movieRatingInput);
+
+        movieImage = response[i].image;
+        movieImgInput.attr('src', movieImage);
+        movieImgInput.addClass('movie-img');
+        movieCard.append(movieImgInput);
+
+        addListener(movieTitle, movieYear, counter);
+    };
+};
+
+// Function that gets the video URL id
+const createYoutubeSection = id => {
+    var movieLink = $('.youTube');
+    var youtubeUrlId = "https://www.youtube.com/embed/" + id;
+    movieLink.attr('src', youtubeUrlId);
+    youtubePreview.removeClass("hidden");
+    movieResult.attr('style','display: none');
+};
+
+// Event listener to take in movie title and calls the youtube API function
+const addListener = (movieTitle, year, counter) => {
+    $(`.movie-${counter}`).on('click', event => {
+        event.preventDefault();
+        clickedTitle = movieTitle;
+        var newYear = year.replaceAll('(', '').replaceAll(')', '');
+
+        if (newYear.includes(' ')) {
+            var newYearSpace = newYear.replaceAll(' ', '%20');
+            getYoutubeApi(clickedTitle, newYearSpace);
+        } else {
+            getYoutubeApi(clickedTitle, newYear);
+        };
+        saveLocal(clickedTitle);
+    })
+};
+
+// Function that saves the titles to local storage with JSON
+const saveLocal = clickedTitle => {
+    if (savedLocal === null) {
+        savedLocal = [clickedTitle];
+        localStorage.setItem("keyCount", JSON.stringify(savedLocal));
+        createHistoryList(clickedTitle);
+        return;
+    } else if (savedLocal.includes(clickedTitle)) {
+        return;
+    } else {
+        savedLocal.push(clickedTitle);
+        localStorage.setItem("keyCount", JSON.stringify(savedLocal));
+    }
+    createHistoryList(clickedTitle);
+};
+
+// Function that creates the history list
+const createHistoryList = clickedTitle => {
+    counter ++
+    var history = $(".history");
+    history.append(`<li class="clickedTitle history-click-${counter}">${clickedTitle}</li>`);
+    historyListener(clickedTitle, counter);
+};
+
+// Function that adds listeners to the titles in history section to open up YouTube video once clicked
+const historyListener = (clickedTitle, counter) => {
+    $(`.history-click-${counter}`).on('click', event => {
+        event.preventDefault();
+        getYoutubeApi(clickedTitle);
+    })
+};
+
+// Function that initializes on refreshing the page and parses the stored array from the local storage through JSON 
+const init = () => {
+    savedTitles = JSON.parse(localStorage.getItem("keyCount"));
+    savedLocal = savedTitles;
+    if (savedTitles === null) {
+        return;
+    } else {
+        var count = 0;
+        var titleMovie;
+        for (var i = 0; i < savedTitles.length; i++) {
+            count++;
+            titleMovie = savedTitles[i];
+            var history = $(".history");
+            history.append(`<li class="clickedTitle history-click-${count}">${titleMovie}</li>`);
+            historyListener(savedTitles[i], count);
+        }
+    };
+};
+
+// Search button click event
+searchButton.on('click', event => {
+    event.preventDefault();
+    var searchInput = $("#search").val();
+    movieInfo.empty();
+    searchMovie(searchInput);
+    youtubePreview.addClass("hidden");
+    movieResult.attr('style','display: flex');
+});
+
+// Function that closes the youtube player when click on cancel
+closeButton.on('click', event => {
+    youtubePreview.addClass("hidden");
+    movieResult.attr('style','display: flex');
+})
+
+// Calling the initialization function when page refreshes
+init();
